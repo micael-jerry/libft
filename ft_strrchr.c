39362e52bcdeba5359b3a0c9b88a0a644d588245
Micael Jerry <hei.jerry.2@gmail.com>
2024-02-23 19:59:04 +0300

    fix: ft strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 13:36:10 by mfidimal          #+#    #+#             */
-/*   Updated: 2024/02/23 10:55:31 by mfidimal         ###   ########.fr       */
+/*   Updated: 2024/02/23 19:56:24 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strrchr(const char *str, int c)
 {
 	int	i;
 
-	i = ft_strlen(str) - 1;
+	i = ft_strlen(str);
 	while (i >= 0)
 	{
 		if ((int)str[i] == c)
