f1f82078de7a291855ba91a047ace580ed5b4d18
Micael Jerry <hei.jerry.2@gmail.com>
2024-02-23 23:44:58 +0300

    bug: without set count

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 21:52:41 by mfidimal          #+#    #+#             */
-/*   Updated: 2024/02/23 23:37:45 by mfidimal         ###   ########.fr       */
+/*   Updated: 2024/02/24 08:50:50 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct str_trim_info
+typedef struct t_str_trim_info
 {
 	int					start;
 	int					end;
-	size_t				len;
-}						str_trim_info;
+}						t_str_trim_info;
 
 static int	is_set(const char c, const char *set)
 {
@@ -35,35 +34,30 @@ static int	is_set(const char c, const char *set)
 	return (0);
 }
 
-static str_trim_info	str_trim_result_len(const char *s1, const char *set)
+static t_str_trim_info	str_trim_result_len(const char *s1, const char *set)
 {
-	size_t			set_count;
 	size_t			i;
 	size_t			j;
-	str_trim_info	trim_info;
+	t_str_trim_info	trim_info;
 
-	set_count = 0;
 	i = 0;
 	while (s1[i] != '\0')
 	{
 		if (is_set(s1[i], set))
-			set_count++;
+			i++;
 		else
 			break ;
-		i++;
 	}
 	trim_info.start = i;
 	j = strlen(s1) - 1;
 	while (j > i)
 	{
 		if (is_set(s1[j], set))
-			set_count++;
+			j--;
 		else
 			break ;
-		j--;
 	}
 	trim_info.end = j;
-	trim_info.len = strlen(s1) - set_count;
 	return (trim_info);
 }
 
@@ -71,10 +65,10 @@ char	*ft_strtrim(const char *s1, const char *set)
 {
 	char			*result;
 	int				i;
-	str_trim_info	info;
+	t_str_trim_info	info;
 
 	info = str_trim_result_len(s1, set);
-	result = (char *)malloc(sizeof(char) * (info.len + 1));
+	result = (char *)malloc(sizeof(char) * ((info.end - info.start + 1) + 1));
 	if (result == NULL)
 		return (NULL);
 	i = 0;
@@ -86,3 +80,11 @@ char	*ft_strtrim(const char *s1, const char *set)
 	result[i] = '\0';
 	return (result);
 }
+
+// int main()
+// {
+//     char test[] = " lorem ipsum dolor sit amet";
+//     printf("\"%s\"\nRES:\n", test);
+//     printf("\"%c\"", ft_strtrim(test, "l ")[90]);
+//     return 0;
+// }
