1a98b0048e4000a892b5f1a213a51b0f14ea57cf
Micael Jerry <hei.jerry.2@gmail.com>
2024-02-24 18:16:12 +0300

    chore: ft putendl fd

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 13:40:17 by mfidimal          #+#    #+#             */
-/*   Updated: 2024/02/24 18:09:41 by mfidimal         ###   ########.fr       */
+/*   Updated: 2024/02/24 18:14:11 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
 typedef struct t_str_trim_info
