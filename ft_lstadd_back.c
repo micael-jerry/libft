de2d4c13b46bcdf30d942317629a30ea4ad838a2
Micael Jerry <hei.jerry.2@gmail.com>
2024-02-25 17:33:19 +0300

    chore: ft lstadd back | fix : front

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -1,24 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/25 15:22:52 by mfidimal          #+#    #+#             */
-/*   Updated: 2024/02/25 17:31:21 by mfidimal         ###   ########.fr       */
+/*   Created: 2024/02/25 16:55:43 by mfidimal          #+#    #+#             */
+/*   Updated: 2024/02/25 17:34:00 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
+	t_list	*actual_last_element;
+
 	if (!lst[0])
 	{
 		lst[0] = new;
 		return ;
 	}
-	new->next = lst[0];
-	lst[0] = new;
+	actual_last_element = ft_lstlast(*lst);
+	actual_last_element->next = new;
 }
