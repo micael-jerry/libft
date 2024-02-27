91036c259a85875e6aab3a84f80c4ef87f3881e3
Micael Jerry Fidimalala <mfidimal@student.42antananarivo.mg>
2024-02-27 17:04:25 +0300

    fix: update ft lstmap

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 13:40:17 by mfidimal          #+#    #+#             */
-/*   Updated: 2024/02/25 21:10:22 by mfidimal         ###   ########.fr       */
+/*   Updated: 2024/02/26 13:48:52 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,6 +83,6 @@ void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
 t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
-					void (*del)(void *));
+						void (*del)(void *));
 
 #endif
