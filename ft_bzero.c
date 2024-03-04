fd1ec214f39f16603161932460292c5de292b9d7
Micael Jerry <hei.jerry.2@gmail.com>
2024-03-04 21:47:18 +0300

    fix: update b_zero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 19:06:17 by mfidimal          #+#    #+#             */
-/*   Updated: 2024/02/23 18:58:26 by mfidimal         ###   ########.fr       */
+/*   Updated: 2024/03/04 21:46:22 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	ft_bzero(void *ptr, size_t n)
 {
-	char	*ptr_trans;
+	unsigned char	*ptr_trans;
 	size_t	i;
 
-	ptr_trans = (char *)ptr;
+	ptr_trans = (unsigned char *)ptr;
 	i = 0;
 	while (i < n)
 	{
