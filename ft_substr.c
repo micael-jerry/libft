5a738cf5f0608a140e4806f207ffefad1780d953
Micael Jerry Fidimalala <mfidimal@student.42antananarivo.mg>
2024-03-06 14:28:39 +0300

    fix: ft substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 20:49:36 by mfidimal          #+#    #+#             */
-/*   Updated: 2024/02/29 14:58:29 by mfidimal         ###   ########.fr       */
+/*   Updated: 2024/03/06 14:26:23 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	size_t	s_len;
 
+	if (!s)
+		return (NULL);
 	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
 	if (s_len < len)
 		len = s_len;
 	sub_str = (char *)malloc(sizeof(char) * (len + 1));
 	if (sub_str == NULL)
 		return (NULL);
 	i = 0;
-	while (i < len && s[i] != '\0')
+	while (i < len && s[i + start] != '\0')
 	{
 		sub_str[i] = s[i + start];
 		i++;
