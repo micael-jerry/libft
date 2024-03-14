42346e785d4fd0b5a02185f4ce74593df08929d3
Micael Jerry <hei.jerry.2@gmail.com>
2024-03-14 18:48:33 +0300

    fix: calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,25 +6,25 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 10:24:04 by mfidimal          #+#    #+#             */
-/*   Updated: 2024/03/06 14:15:37 by mfidimal         ###   ########.fr       */
+/*   Updated: 2024/03/14 18:47:58 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t n, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
-	char	*mem;
+	void	*mem;
 	size_t	i;
 
-	mem = (char *)malloc(size * n);
+	mem = malloc(count * size);
 	if (!mem)
 		return (NULL);
 	i = 0;
-	while (i < n)
+	while (i < (count * size))
 	{
-		mem[i] = 0;
+		((char *)mem)[i] = 0;
 		i++;
 	}
-	return ((void *)mem);
+	return (mem);
 }
